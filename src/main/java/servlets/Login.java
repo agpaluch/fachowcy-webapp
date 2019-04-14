16382457f72651535972a43ebcce5460fbf14bd2
arekk91 <arek.kotula91@gmail.com>
2019-04-14 20:43:49 +0200

    additional servlets

---
+++ src/main/java/servlets/Login.java
@@ -0,0 +1,51 @@
+package servlets;
+
+import freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+
+    Logger logger = Logger.getLogger(getClass().getName());
+    Template template;
+
+    @Override
+    public void init() {
+        try {
+            template = TemplateProvider.createTemplate(getServletContext(), "login.ftlh");
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html; charset=utf-8");
+
+        Map<String, String> map = new HashMap<>();
+
+        map.put("", "");
+
+        try {
+            template.process(map, resp.getWriter());
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
