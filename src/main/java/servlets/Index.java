7962e07176c03cc9ec9ff4cc6b4a55c1c95542a6
arekk91 <arek.kotula91@gmail.com>
2019-06-13 06:21:39 +0200

    Embedded User, DAO adjustment, cleanup

--- src/main/java/servlets/Index.java
+++ src/main/java/servlets/Index.java
@@ -54,27 +54,6 @@ public class Index extends HttpServlet {
             logger.log(Level.SEVERE, e.getMessage(), e);
         }
 
-        UserDetails userDetails = UserDetails.builder()
-                //.userLogin(userLogin)
-                .name("aaa")
-                .surname("bbb")
-                .profession(TypeOfProfession.ELECTRICIAN)
-                .phoneNumber(600000000)
-                .city(City.WARSZAWA)
-                .longitude(6.6)
-                .latitude(7.7)
-                .build();
-        UserLogin userLogin = UserLogin.builder()
-                .userDetails(userDetails)
-                .email("fuckme@abc.xom")
-                .password("password")
-                .role(Role.ADMIN)
-                .build();
-        //userLogin.setPassword("Password");
-        userLoginDAO.save(userLogin);
-
-
-
     }
 
     @Override
