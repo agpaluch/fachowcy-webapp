4a470e6e7d895442324ec6948bb0c1328cdbacb9
arekk91 <arek.kotula91@gmail.com>
2019-07-16 08:42:43 +0200

    soap started

---
+++ src/main/java/servlets/ContactUs.java
@@ -0,0 +1,60 @@
+package servlets;
+
+import config.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import session.SessionInfo;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@WebServlet("/contact-us")
+public class ContactUs extends HttpServlet {
+
+    private static final String TEMPLATE_NAME = "index";
+    Logger logger = Logger.getLogger(getClass().getName());
+    Template template;
+
+    @Inject
+    SessionInfo sessionInfo;
+
+    @Override
+    public void init() {
+        try {
+            template = TemplateProvider.createTemplate(getServletContext(), TEMPLATE_NAME);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Map<String, Object> dataMap = new HashMap<>();
+        dataMap.put("content", "contact-us");
+        dataMap.put("sessionInfo", sessionInfo);
+
+        try {
+            template.process(dataMap, resp.getWriter());
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.sendRedirect("/index");
+
+    }
+}
