2ed816dc7363ab372ea5d3a1c02d8f271b2803ef
Agnieszka <agnieszka.paluch90@gmail.com>
2020-02-11 20:32:02 +0100

    Improving TemplateUtil class

--- src/main/java/template/TemplateProvider.java
+++ src/main/java/template/TemplateProvider.java
@@ -3,16 +3,19 @@ package template;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContext;
 import java.io.IOException;
 
 public class TemplateProvider {
 
+    private static final Logger logger = LoggerFactory.getLogger(TemplateProvider.class);
     public static final String TEMPLATES_DIRECTORY_PATH = "/fm-templates";
     private static final String TEMPLATE_EXT = ".ftlh";
 
-    public static Template createTemplate(ServletContext servletContext, String templateName) throws IOException {
+    public static Template createTemplate(ServletContext servletContext, String templateName) {
         Configuration configuration = new Configuration(Configuration.VERSION_2_3_28);
 
         configuration.setServletContextForTemplateLoading(servletContext, TEMPLATES_DIRECTORY_PATH);
@@ -21,6 +24,12 @@ public class TemplateProvider {
         configuration.setLogTemplateExceptions(false);
         configuration.setWrapUncheckedExceptions(true);
 
-        return configuration.getTemplate(templateName + TEMPLATE_EXT);
+        try {
+            return configuration.getTemplate(templateName + TEMPLATE_EXT);
+        } catch (IOException e){
+            logger.error("Could not create template: "+templateName, e);
+            throw new IllegalStateException(e);
+        }
+
     }
 }
--- src/main/java/template/TemplateUtil.java
+++ src/main/java/template/TemplateUtil.java
@@ -2,35 +2,33 @@ package template;
 
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
 
+@RequiredArgsConstructor
 public class TemplateUtil {
 
+    private static final Logger logger = LoggerFactory.getLogger(TemplateUtil.class);
+    private final Template template;
 
-    static void freemarkerEngine(Map<String, Object> dataMap, Template template, PrintWriter printWriter, Logger logger) {
+    public void freemarkerEngine(Map<String, Object> dataMap, HttpServletResponse resp) {
 
         try {
-            template.process(dataMap, printWriter);
+            this.template.process(dataMap, resp.getWriter());
         } catch (TemplateException | IOException e) {
-            logger.error(e.getMessage());
+            logger.error("Could not process template", e);
+            throw new IllegalArgumentException(e);
         }
 
     }
 
-    public static Template templateFactory(ServletContext context, String templateName, Logger logger) {
-        Template template = null;
-        try {
-            template = TemplateProvider.createTemplate(context, templateName);
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-        return template;
-    }
 
 
 }
