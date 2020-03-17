9fe8a89f31ae93e5ba3746ec4cf29d7e4aad423f
Agnieszka <agnieszka.paluch90@gmail.com>
2020-03-17 08:26:06 +0100

    Changes in DatasourceProvider

--- src/main/java/config/DatasourceProvider.java
+++ src/main/java/config/DatasourceProvider.java
@@ -5,9 +5,10 @@ import javax.ejb.Singleton;
 
 @Singleton
 @DataSourceDefinition(name = "java:app/fachmann",
-        className = "com.mysql.cj.jdbc.MysqlXADataSource",
-        user = "root",
-        password = "password",
-        url = "jdbc:mysql://localhost:3306/fachmann?useSSL=false")
+        className = "org.h2.jdbcx.JdbcDataSource",
+        user = "sa",
+        password = "",
+        url = "jdbc:h2:mem:test;DB_CLOSE_ON_EXIT=FALSE;INIT=runscript from '/home/agnieszka/Desktop/fachowcy-webapp/src/main/resources/db.migration/V1__create_database.sql'")
+       /* url = "jdbc:h2:mem:fachmann;DB_CLOSE_DELAY=-1")*/
 public class DatasourceProvider {
 }
--- src/main/resources/db.migration/V1__create_database.sql
+++ src/main/resources/db.migration/V1__create_database.sql
@@ -1,15 +1,16 @@
-CREATE SCHEMA IF NOT EXISTS testDB;
-USE testDB;
+/*CREATE SCHEMA IF NOT EXISTS fachmann;
+USE fachmann;*/
 
 /*Create table for professions*/
 CREATE TABLE IF NOT EXISTS Professions
 (
-  id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  /*id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,*/
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
   profession VARCHAR(100)
 );
 
 
-INSERT INTO testDB.Professions (profession) VALUES
+INSERT INTO Professions (profession) VALUES
 ('hydraulik'),
 ('elektryk');
 
@@ -20,28 +21,31 @@ INSERT INTO testDB.Professions (profession) VALUES
 /*Create table for user data (login and details) and connect it with professions*/
 CREATE TABLE IF NOT EXISTS userData
 (
-    id          BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+ /*   id          BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,*/
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT,
     email       VARCHAR(100) UNIQUE NOT NULL,
     password    VARCHAR(100)        NOT NULL,
     role        VARCHAR(32) NOT NULL,
     signUpDate DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP,
     name          VARCHAR(100)      NOT NULL,
     surname       VARCHAR(100)      NOT NULL,
-    profession_id  BIGINT UNSIGNED,
+    profession_id  BIGINT,
+    /*profession_id  BIGINT UNSIGNED,*/
     phoneNumber   BIGINT(14)        NOT NULL,
     city          VARCHAR(100)   NOT NULL,
     longitude     DOUBLE   NOT NULL,
     latitude      DOUBLE    NOT NULL,
-    numberOfLikes INT UNSIGNED NOT NULL
+    numberOfLikes INT NOT NULL
+   /* numberOfLikes INT UNSIGNED NOT NULL*/
 );
 
 ALTER TABLE userData ADD FOREIGN KEY (profession_id) REFERENCES Professions (id);
 
 
-ALTER TABLE userData CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+/*ALTER TABLE userData CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+*/
 
-
-INSERT INTO testDB.userData (email, password, role, name, surname, profession_id,
+INSERT INTO userData (email, password, role, name, surname, profession_id,
 phoneNumber, city, longitude, latitude, numberOfLikes) VALUES
     ('client1@gmail.com', 'client1', 'CLIENT', 'Jan', 'Kowalski',
     null, 507654321, 'WARSZAWA', 51, 21.7, 2),
@@ -62,13 +66,16 @@ phoneNumber, city, longitude, latitude, numberOfLikes) VALUES
 
 /*create table for storing messages*/
 
-CREATE TABLE IF NOT EXISTS testDB.Messages
+CREATE TABLE IF NOT EXISTS Messages
 (
-    id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    /*id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,*/
     message    VARCHAR(400) NOT NULL,
     wasRead    BOOLEAN DEFAULT '0',
-    sender_id     BIGINT UNSIGNED,
-    recipient_id  BIGINT UNSIGNED
+    sender_id     BIGINT,
+    recipient_id  BIGINT
+  /*sender_id     BIGINT UNSIGNED,
+    recipient_id  BIGINT UNSIGNED*/
 );
 
 ALTER TABLE Messages
@@ -87,9 +94,12 @@ INSERT INTO Messages (message, sender_id, recipient_id) VALUES
 
 CREATE TABLE IF NOT EXISTS Comment
 (
- id BIGINT UNSIGNED UNIQUE PRIMARY KEY AUTO_INCREMENT,
+ id BIGINT PRIMARY KEY AUTO_INCREMENT,
+ opinion_maker_id BIGINT,
+ recipient_id BIGINT,
+/* id BIGINT UNSIGNED UNIQUE PRIMARY KEY AUTO_INCREMENT,
  opinion_maker_id BIGINT UNSIGNED,
- recipient_id BIGINT UNSIGNED,
+ recipient_id BIGINT UNSIGNED,*/
  opinion VARCHAR (400) NOT NULL
 );
 
@@ -102,4 +112,5 @@ INSERT INTO Comment (opinion_maker_id, recipient_id, opinion) VALUES
 (1, 2, 'Dobra robota.'),
 (1, 1, 'Dobra robota.');
 
-ALTER DATABASE testDB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+/*ALTER DATABASE fachmann CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+*/
